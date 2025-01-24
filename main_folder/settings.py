7da3e4af6cdd86ceb9007b47cd20eb024b9435e6
niraj-commits <karki.ni018@gmail.com>
2025-01-24 10:08:58 +0545

    Changed Database Config

--- main_folder/settings.py
+++ main_folder/settings.py
@@ -78,15 +78,11 @@ WSGI_APPLICATION = 'main_folder.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'restaurant_app',
-        'USER': 'karkibaby',
-        'PASSWORD': 'niraj',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '5432'
     }
